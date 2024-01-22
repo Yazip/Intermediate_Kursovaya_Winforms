cf14d6d884b760ea791d6bb48e38c88ff8543078
Ilya Baretkin <usaasu386@mail.ru>
2024-01-22 17:56:01 +0700

    Intermediate version

---
+++ BeatTheMole/Mole.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BeatTheMole
+{
+    internal class Mole : Enemy
+    {
+        public Mole(PictureBox[] sprites)
+        {
+            this.sprites = (PictureBox[])sprites.Clone();
+        }
+        public void StartMoleAnimation(int interval)
+        {
+            Animation mole_animation = new Animation(sprites, interval);
+            mole_animation.StartAnimation();
+        }
+    }
+}
